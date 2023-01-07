825ec2f5b5195544bf98b9af612f81b571dbe16e
dans-temp <danielgoresht+1@gmail.com>
2023-01-07 10:52:34 -0700

    progu

--- game/globals.js
+++ game/globals.js
@@ -1,5 +1,5 @@
-export const WIDTH_CANVAS = 800;
-export const HEIGHT_CANVAS = 600;
+export const WIDTH_CANVAS = 1600;
+export const HEIGHT_CANVAS = 1200;
 export const PADDING_CANVAS = 20;
 
 export const FONT_TITLE = "JosefinSlab";
--- game/scenes/gameplay.js
+++ game/scenes/gameplay.js
@@ -14,7 +14,7 @@ const LEVELS =
             [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [2, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -28,16 +28,42 @@ const LEVELS =
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+		],
+		crops:
+		[
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 		]
 	}
 ]
 
+const CROP_ABILITIES = ["doublejump","dash"]
 
 // PHYSICS
 const RUN_ACCEL = 0.1;
 const RUN_DECEL = 0.05;
 const MAX_SPEED = 2;
 const JUMPSPEED = 3.4;
+const DASHSPEED = 12;
+const DASH_DECEL = 1;
 const GRAVITY = 0.15;
 
 // TILES
@@ -52,8 +78,16 @@ const dude = {
     hp_max: 5,
     hp_current: 5,
 	falling: true,
+	facing_right: true,
+	attack_range: 2,
+	attack_cooldown: 500,
+	ability_cooldown: 500,
+	ability: "none",
     level: 1
 };
+
+let next_attack = dude.attack_cooldown;
+let next_abiility = dude.ability_cooldown
 
 export default new Phaser.Class({
 	Extends: Phaser.Scene,
@@ -85,12 +119,12 @@ export default new Phaser.Class({
 			}
 		}
 
-		this.cursors = this.input.keyboard.addKeys("UP,LEFT,DOWN,RIGHT,W,A,S,D,R,SPACE");
+		this.cursors = this.input.keyboard.addKeys("UP,LEFT,DOWN,RIGHT,W,A,S,D,E,R,SPACE");
 
 		dude.sprite = this.add.sprite(50, 50, 'dude').setDisplaySize(20, 16).setOrigin(0.5, 1).setDepth(4);
 		this.cameras.main.startFollow(dude.sprite);
 		this.cameras.main.setBounds(0, 0, LEVELS[0].width*TILE_SIZE, LEVELS[0].height*TILE_SIZE);
-		this.cameras.main.setZoom(1);
+		this.cameras.main.setZoom(2);
 	},
 	update()
 	{
@@ -98,6 +132,8 @@ export default new Phaser.Class({
 		const right = this.cursors.D.isDown || this.cursors.RIGHT.isDown;
 		const jump = this.cursors.W.isDown || this.cursors.UP.isDown;
 		const down = this.cursors.S.isDown || this.cursors.DOWN.isDown;
+		const use_ability = this.cursors.E.isDown;
+		const attack = this.cursors.SPACE.isDown;
 
 		if( (!left && !right) || (left && right))
 		{
@@ -109,11 +145,24 @@ export default new Phaser.Class({
 
 		if (left && !right)
 		{
-			dude.xvel = Math.max(-MAX_SPEED, dude.xvel - RUN_ACCEL);
+			if(Math.max(-MAX_SPEED, dude.xvel - RUN_ACCEL) < dude.xvel)
+				dude.xvel = Math.max(-MAX_SPEED, dude.xvel - RUN_ACCEL);
+			dude.facing_right = false;
 		}
-		else if (right && !left)
+		else if(right && !left)
 		{
-			dude.xvel = Math.min(MAX_SPEED, dude.xvel + RUN_ACCEL);
+			if(Math.min(MAX_SPEED, dude.xvel + RUN_ACCEL) > dude.xvel)
+				dude.xvel = Math.min(MAX_SPEED, dude.xvel + RUN_ACCEL);
+			dude.facing_right = true;
+		}
+
+		//dash decel
+		if(Math.abs(dude.xvel) > MAX_SPEED)
+		{
+			if(dude.xvel > 0)
+				dude.xvel -= DASH_DECEL;
+			else
+				dude.xvel += DASH_DECEL;
 		}
 		
 		dude.x_old = dude.sprite.x;
@@ -127,6 +176,18 @@ export default new Phaser.Class({
 		{
 			dude.yvel = -JUMPSPEED;
 			dude.falling = true;
+		}
+
+		if(attack && this.time.now > next_attack)
+		{
+			attacks(this);
+			next_attack = this.time.now + dude.attack_cooldown;
+		}
+
+		if(use_ability && dude.ability != "none" && this.time.now > next_abiility)
+		{
+			ability(this);
+			next_abiility = this.time.now + dude.ability_cooldown;
 		}
 
 		dude.sprite.x += dude.xvel;
@@ -164,17 +225,75 @@ export default new Phaser.Class({
 		const index_col_left = Math.floor((dude.sprite.x - dude.width/2)/TILE_SIZE);
 		const index_col_right = Math.floor((dude.sprite.x + dude.width/2 - EPSILON)/TILE_SIZE);
 
-		if(!LEVELS[0].tiles[index_row][index_col_left] && !LEVELS[0].tiles[index_row + 1][index_col_right])
+		if(index_row + 1 >= LEVELS[0].height)
+		{
+			//player hit bottom
+			console.log("dead");
+		}
+		else if(!LEVELS[0].tiles[index_row][index_col_left] && !LEVELS[0].tiles[index_row + 1][index_col_right])
 		{
 			dude.falling = true;
 		}
 	}
 });
 
+function attacks(game)
+{
+	//x range
+	let min_range_x;
+	let max_range_x;
+	if(dude.facing_right)
+	{
+		min_range_x = Math.floor((dude.sprite.x)/TILE_SIZE);
+		max_range_x = Math.floor((dude.sprite.x)/TILE_SIZE) + dude.attack_range;
+		
+	}
+	else
+	{
+		min_range_x = Math.floor((dude.sprite.x)/TILE_SIZE) - dude.attack_range;
+		max_range_x = Math.floor((dude.sprite.x)/TILE_SIZE);
+	}
+	//check for hit
+	for(let i = 0; i < max_range_x - min_range_x; i ++)
+	{
+		console.log(Math.floor(dude.sprite.y/TILE_SIZE))
+		if(LEVELS[0].tiles[Math.floor((dude.sprite.y-(dude.height/2))/TILE_SIZE)][min_range_x + i])
+		{
+			console.log('strike wall');
+		}
+		const crop_index = LEVELS[0].crops[Math.floor((dude.sprite.y-(dude.height/2))/TILE_SIZE)][min_range_x + i]
+		if(crop_index)
+		{
+			console.log('hit crop');
+			dude.ability = CROP_ABILITIES[crop_index - 1];
+			LEVELS[0].crops[Math.floor((dude.sprite.y-(dude.height/2))/TILE_SIZE)][min_range_x + i] = 0;
+		}
+	}
+};
+
+function ability(game)
+{
+	console.log(dude.ability)
+	if(dude.ability == "doublejump")
+	{
+		dude.yvel = -JUMPSPEED;
+		dude.falling = true;
+	}
+	else if(dude.ability == "dash")
+	{
+		if(dude.facing_right)
+		{
+			dude.xvel += DASHSPEED;
+		}
+		else
+		{
+			dude.xvel -= DASHSPEED;
+		}
+	}
+}
+
 function handleCollision(game)
 {
-
-	
 	function collideTile(index_row, index_col)
 	{
 		const y_top = index_row*TILE_SIZE;
@@ -237,12 +356,8 @@ function handleCollision(game)
 
 	// bottom right
 	index_col = Math.floor((dude.sprite.x + dude.width/2 - EPSILON)/TILE_SIZE);
-	console.log(index_col);
 	if(LEVELS[0].tiles[index_row][index_col])
-	{
-		console.log("bottom right");
 		collideTile(index_row, index_col);
-	}
 
 	// top left again
 	index_row = Math.floor((dude.sprite.y - dude.height)/TILE_SIZE);
